61e411ea2ec713bc59c95843a69ae88e75c4a890
Bianca Dogaru <biancadogaru677@gmail.com>
2026-09-05 14:29:41 -0400

    C3 - constructors, getters and setters to Person Class

--- Person.java
+++ Person.java
@@ -6,12 +6,43 @@ public class Person{
     private int age;
 
     //constructors
-    public Person person(String firstName; String lastName; int age){
+    public Person(String firstName, String lastName, int age){
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
 
-        return person;
+    }
+
+    public Person(Person other){
+        this(other.firstName, other.lastName, other.age);
+    }
+
+    public Person(){
+        this.firstName = "No first name";
+        this.lastName = "No last name";
+        this.age = 0;
+    }
+
+    //getters
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lasttName;
+    }
+    public int getAge(){
+        return this.age;
+    }
+
+    //setters
+    public void setFirstName(String fn){
+        this.firstName = fn;
+    }
+    public void setLastName(String ln){
+        this.lastName = ln;
+    }
+    public void setAge(int a){
+        this.age = a;
     }
 
 }
